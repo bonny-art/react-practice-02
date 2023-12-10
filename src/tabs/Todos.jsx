bb5b7e35cb3f375b5bea166db1ac1d54a86b8e03
@7Karina <karinafrank750@gmail.com>
2023-12-10 16:33:13 +0200

    add todo

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,29 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+  };
+  addTodo = text => {
+    this.setState(prev => {
+      return { todos: [...prev.todos, { text, id: nanoid() }] };
+    });
+  };
+
   render() {
-    return <Text>Todos</Text>;
+    return (
+      <>
+        <SearchForm onSubmit={this.addTodo} />
+        <Grid>
+          {this.state.todos.map(({ text, id }, idx) => {
+            return (
+              <GridItem key={id}>
+                <Todo text={text} count={idx + 1} />
+              </GridItem>
+            );
+          })}
+        </Grid>
+      </>
+    );
   }
 }
